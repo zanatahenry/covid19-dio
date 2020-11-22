15e8352edc98be61d49e663a023c0f851d7ac91f
Henry Zanata <henryzanata@hotmail.com>
2020-11-22 20:32:40 -0300

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import { Card, CardContent } from './Card'
+import Button from './Button'
+import Grid from './Grid'
+import Typography from './Typography'
+import Skeleton from './Skeleton'
+import Select from './Select'
+import MenuItem from './MenuItem'
+
+export {
+  Button, 
+  CardContent,
+  Card,
+  Grid,
+  Typography,
+  Skeleton,
+  Select,
+  MenuItem
+}
